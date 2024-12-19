d2772b95093cb2f2501cc0db09b28af8beb132f0
Luan <luanmarciell1200@gmail.com>
2024-12-18 23:18:54 -0400

    Alterando funções

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = []
+
 def exibir_nome_do_programa():
  print ('𝑺𝒂𝒃𝒐𝒓 𝑬𝒙𝒑𝒓𝒆𝒔𝒔\n')
 
@@ -18,11 +20,21 @@ def opcao_invalida():
     input('Digite uma tecla para voltar ao menu principal!')
     main()
 
+def cadastrar_novo_restaurante():
+    os.system('cls')
+    print('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar:')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    input = ('Digite uma tecla para voltar ao menu principal')
+    main()
+  
 def escolher_opcao():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
+
         if opcao_escolhida == 1:
-            print ('Cadastrar restaurante')
+            cadastrar_novo_restaurante()
         elif opcao_escolhida == 2:
             print ('Listar restaurante')
         elif opcao_escolhida == 3:
